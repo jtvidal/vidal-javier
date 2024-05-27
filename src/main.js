014fe7edd4922bca28cc2bba42a9af7ed9321e13
jvidal <javier.vidal@davinci.edu.ar>
2024-05-27 00:24:02 -0300

    feat: Atlas icons installed

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import './assets/main.css'
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import "./assets/main.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Atlas from "@vectopus/atlas-icons-vue";
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(Atlas);
+app.mount("#app");
