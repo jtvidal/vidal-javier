e0a1b678d8bfaf78a04cf1c4835674b5120478e2
jvidal <javier.vidal@davinci.edu.ar>
2024-05-27 00:11:48 -0300

    change: PostModel for PostsCard

---
+++ src/components/PostsCard.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  name: "PostsCard",
+  props: { postObject: Object },
+  data() {
+    return {
+      postCard: this.postObject,
+    };
+  },
+  async mounted() {
+    console.log("PostCard: ", this.$props.postObject);
+    await this.loadPostCard(this.$props.postObject);
+  },
+  methods: {
+    async loadPostCard(post) {
+      this.postCard = await post;
+    },
+  },
+};
+</script>
+<template>
+  <div
+    class="shadow-md shadow-zinc-400 pt-4 flex flex-col items-center border-2 border-primary rounded-lg"
+  >
+    <h3>{{ postCard.title }}</h3>
+    <div class="p-4">
+      <p>{{ postCard.content }}</p>
+    </div>
+    <div class="flex w-full justify-center">
+      <div class="w-1/2">
+        <button
+          class="w-full p-1 bg-primary hover:bg-opacity-80 hover:text-zinc-100"
+        >
+          Like
+        </button>
+      </div>
+      <div class="w-1/2">
+        <button
+          class="w-full p-1 bg-primary hover:bg-opacity-80 hover:text-zinc-100"
+        >
+          Comment
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
