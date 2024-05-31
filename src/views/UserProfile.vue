c625a4dafb90f7598b782e271c624a93d8445644
jvidal <javier.vidal@davinci.edu.ar>
2024-05-31 20:51:24 -0300

    Update UserProfile.vue

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,1 +1,13 @@
 <!-- TODO: view for a generic User in db accessible from other users -->
+<script>
+export default {
+  name: "UserProfile",
+  data() {
+    return {
+      userData: {
+        
+      },
+    };
+  },
+};
+</script>
