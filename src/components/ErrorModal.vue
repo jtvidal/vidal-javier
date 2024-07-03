f575771917568845ffde0d687f1af38c2271577a
jvidal <javier.vidal@davinci.edu.ar>
2024-07-03 14:47:39 -0300

    feat: ErrorModal will be MessageModal

--- src/components/ErrorModal.vue
+++ src/components/ErrorModal.vue
@@ -1,11 +1,12 @@
 <script>
 export default {
   name: "ErrorModal",
-  props: { errorDescription: String },
+  props: { errorDescription: String, succeed: Boolean },
   data() {
     return {
       message: this.$props.errorDescription,
       close: true,
+      succeeded: this.succeed,
     };
   },
   methods: {
@@ -20,8 +21,13 @@ export default {
   <div
     class="fixed flex items-center bg-zinc-950 bg-opacity-70 top-0 bottom-0 right-0 left-0"
   >
-    <div class="mx-auto max-w-80 flex flex-col gap-4 bg-zinc-100 p-4 rounded-lg border-4 border-red-600">
-      <div class="self-center hover:font-semibold ease-in-out duration-150 "><button @click="closeModal">Close</button></div>
+    <div
+      :class="succeeded ? 'border-green-500' : 'border-red-600'"
+      class="mx-auto max-w-80 flex flex-col gap-4 bg-zinc-100 p-4 rounded-lg border"
+    >
+      <div class="self-center hover:font-semibold ease-in-out duration-150">
+        <button @click="closeModal">Close</button>
+      </div>
       <div>
         <p class="text-center uppercase font-bold">{{ message }}</p>
       </div>
